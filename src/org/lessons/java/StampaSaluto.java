8a86ee65b2787120692333b53401e2c8e7c8ece5
mlietti1 <mcf.lietti@outlook.com>
2023-03-07 14:30:15 +0100

    last commit

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -2,6 +2,6 @@ package org.lessons.java;
 
 public class StampaSaluto {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Ciao Java!");
     }
 }
